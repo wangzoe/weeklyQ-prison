dd0a9f488cdb47e4d6e08ef28c74aacc5452d861
wangzoe <zoewang.z@qq.com>
2017-03-31 23:15:35 +0800

    expectation of all situations

---
+++ expectation_for_prison.py
@@ -0,0 +1,75 @@
+# for weekly Q: prisoner delima
+
+from random import randint
+from statistics import mean
+
+def prison_delima(n, strategy1, strategy2):
+    suspect1=0
+    suspect2=0
+    while n > 0:
+        if strategy1=="nice" and strategy2=='nice':
+            suspect1 += 1
+            suspect2 += 1
+            n -= 1
+        elif strategy1=='nice' and strategy2=='rat':
+            suspect1 += 5
+            suspect2 += 0
+            n -= 1
+        elif strategy1=='nice' and strategy2=='tit_for_tat':
+            suspect1 += 1
+            suspect2 += 1
+            strategy2 = 'nice'
+            n -= 1
+        elif strategy1=='rat' and strategy2=='nice':
+            suspect1 += 0
+            suspect2 += 5
+            n -= 1
+        elif strategy1=='rat' and strategy2=='rat':
+            suspect1 += 2
+            suspect2 += 2
+            n -= 1
+        elif strategy1=='rat' and strategy2=='tit_for_tat':
+            suspect1 += 0
+            suspect2 += 5
+            strategy2 = 'rat'
+            n -= 1
+        elif strategy1=='tit_for_tat' and strategy2=='nice':
+            suspect1 += 1
+            suspect2 += 1
+            strategy1 = 'nice'
+            n -= 1
+        elif strategy1=='tit_for_tat' and strategy2=='rat':
+            suspect1 += 5
+            suspect2 += 0
+            strategy1 = 'rat'
+            n -=1
+        elif strategy1=='tit_for_tat' and strategy2=='tit_for_tat':
+            suspect1 += 1
+            suspect2 += 1
+            strategy1 = 'nice'
+            strategy2 = 'nice'
+            n -= 1
+
+    return (suspect1, suspect2)
+
+def expectation_of_certen_type(certen_type, n, times=100):
+    types = ['nice', 'rat', 'tit_for_tat']
+    years_for_certen=[]
+    years_for_random=[]
+    years=[]
+    for t in range (0,times):
+        strategy1 = certen_type
+        strategy2 = types[randint(0,2)]
+        years=prison_delima(n, strategy1, strategy2)
+        years_for_certen.append(years[0])
+        years_for_random.append(years[1])
+    expectation_for_certen = mean(years_for_certen)
+    expectation_for_random = mean(years_for_random)
+    max_year_for_certen = max(years_for_certen)
+    max_year_for_random = max(years_for_random)
+    sum_of_both = mean(years_for_certen) + mean(years_for_random)
+
+    return(expectation_for_certen, expectation_for_random,
+           max_year_for_certen, max_year_for_random, sum_of_both)
+        
+
